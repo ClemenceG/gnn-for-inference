cbf1b38bd8c15bd3829a216767e25b4e906a2fa5
AnthonyGosselin <anthony.gosselin23@gmail.com>
2023-11-23 13:31:46 -0500

    Removed submodule

--- pull_sunfanyunn_repo.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -ex
-
-# Download code from https://github.com/sunfanyunn/pgm_graph_inference - original code
-git clone https://github.com/sunfanyunn/pgm_graph_inference.git
-# git subtree add https://github.com/sunfanyunn/pgm_graph_inference.git
